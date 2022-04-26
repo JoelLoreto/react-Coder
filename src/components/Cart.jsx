e61c67cb4388d4556ee711a40dd5302e0c9999c5
Joel <joelivan.loretogmail.com>
2022-04-25 22:59:08 -0300

    entrega clase 10

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { CartContext } from "./Context/CartContext";
 
 const Cart = () => {
@@ -9,12 +10,13 @@ const {cart, removeItem, clear} = useContext(CartContext)
         <h1>Tus productos en el carrito son:</h1>
         <h2>{cart.length} Items</h2>
 
-        {cart.length > 0 && cart.map((item) =>(
+        {cart.length === 0 ?  <button> <Link to={"/"} >No hay productos en tu carrito, ir a comprar</Link></button> :cart.length > 0 && cart.map((item) =>(
             <div key={item.id}>
             <img src={item.image} alt="" />
             <span>{item.name}</span>
             <button onClick={()=>removeItem(item.id)}> Remover</button>
             <button onClick={clear}> Vaciar Carrito</button>
+            <button onClick={clear}> Terminar compra</button>
 
             </div>
         ))}
